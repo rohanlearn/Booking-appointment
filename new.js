571045d46daa1d463a13e50d2e9665f1cbad0bf0
B Rohan Rao <109647650+rohanlearn@users.noreply.github.com>
2023-04-22 13:52:43 +0530

    deletebtn

--- new.js
+++ new.js
@@ -8,9 +8,13 @@ const form = document.querySelector("form")
     })
     // var forum = document.querySelector('form');
 var adder = document.getElementById('add');
-var input = document.querySelector('#name');
- form.addEventListener('submit',addtostorage);
- function addtostorage(e){   
+var dele = document.getElementById("delete");
+
+adder.addEventListener('click',deletion);
+form.addEventListener('submit',addtostorage);
+
+
+function addtostorage(e){   
       e.preventDefault()
       let name= document.getElementById('name').value;
     var email = document.getElementById('email').value;
@@ -23,9 +27,22 @@ var input = document.querySelector('#name');
    object = JSON.parse(object)
    var p = document.createElement("p");
    var data = document.createTextNode("Name: "+object.name+",   Email: "+object.email) 
+   var dltbtn = document.createElement("button");
+   dltbtn.id="delete";
+   dltbtn.innerText="Delete"
+   dltbtn.style.marginLeft="10px"
    p.appendChild(data)
+   p.appendChild(dltbtn)
    adder.appendChild(p);
-   console.log(object.name)
-   
 
  }
+
+function deletion(e){
+
+ console.log(e.target.id)
+ if (e.target.id=="delete"){
+    var p = e.target.parentElement;
+    adder.removeChild(p);
+
+ }
+}
